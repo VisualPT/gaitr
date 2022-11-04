f3d19f085e0eed99ede8b59750e89bb48f646cdf
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2022-11-04 06:35:56 -0400

    email integration with half-finished patient model

---
+++ lib/data_helper.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:typed_data';
+
+import 'package:gaiter/models/patient_data.dart';
+
+void calculateFallRisk(double seconds) {
+  double risk = (seconds - 7) / 20;
+  patientData.fallRisk = risk.clamp(0.1, 1.0);
+}
+
+///Input string is in format MM/DD/YYYY , returns a DateTime instance
+stringToDateTime(String birthdate) {
+  try {
+    final int year = int.parse(
+        birthdate.substring(birthdate.lastIndexOf('/') + 1, birthdate.length));
+    final int month = int.parse(birthdate.substring(
+        birthdate.indexOf('/') + 1, birthdate.lastIndexOf('/')));
+    final int day = int.parse(birthdate.substring(0, birthdate.indexOf('/')));
+    return DateTime(year, month, day);
+  } catch (e) {
+    log(e.toString());
+  }
+}
+
+String uint8ListTob64(Uint8List uint8list) {
+  String base64String = base64Encode(uint8list);
+  String header = 'data:application/pdf;base64,';
+  return header + base64String;
+}
