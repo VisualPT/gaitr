3f7b9c1fd4d0df86c8c5f20888f87ae8fd947d68
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2022-11-10 23:28:09 +0100

    grrrrrr

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -24,7 +24,7 @@ brew install cocoapods
 cd ios && pod install # run `pod install` in the `ios` directory.
 
 cd ..
-export PATH="/usr/local/bin/amplify:$PATH"
+export PATH="$PATH:$HOME/amplify/bin"
 # Get amplify configuration file
 amplify pull --appId d2hh5wki5gcwzi --envName staging
 
